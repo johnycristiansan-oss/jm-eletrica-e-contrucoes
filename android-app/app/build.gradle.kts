ae1d1e9be8eafea3c5abd51203a2adb54421d5fe
johnycristiansan-oss <johnycristiansan@gmail.com>
2026-08-17 18:22:54 -0300

    Simplifica dependencias para build inicial do APK

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -18,10 +18,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 }
@@ -29,6 +25,4 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.10.0")
-    implementation("androidx.webkit:webkit:1.12.1")
 }
