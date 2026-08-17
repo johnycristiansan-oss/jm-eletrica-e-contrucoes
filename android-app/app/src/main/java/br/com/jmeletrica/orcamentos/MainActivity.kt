78375c5990f8a117ad5cfb17b4263f71d695d8b5
johnycristiansan-oss <johnycristiansan@gmail.com>
2026-08-17 16:12:11 -0300

    Adiciona Activity principal do app Android

---
+++ android-app/app/src/main/java/br/com/jmeletrica/orcamentos/MainActivity.kt
@@ -0,0 +1,51 @@
+package br.com.jmeletrica.orcamentos
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = false
+        webView.settings.allowContentAccess = false
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.setBackgroundColor(0xFFF5F7FA.toInt())
+
+        setContentView(webView)
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://raw.githubusercontent.com/johnycristiansan-oss/jm-eletrica-e-contrucoes/main/orcamento/index.html")
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) webView.goBack() else finish()
+            }
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
+    }
+}
