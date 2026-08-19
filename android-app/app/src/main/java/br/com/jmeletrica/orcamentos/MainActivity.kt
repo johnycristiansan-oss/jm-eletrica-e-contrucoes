cd7749413c8d2049cefffb41ae5d8abaa0abab24
johnycristiansan-oss <johnycristiansan@gmail.com>
2026-08-19 20:20:22 -0300

    Implementa logo oficial no app Android

--- android-app/app/src/main/java/br/com/jmeletrica/orcamentos/MainActivity.kt
+++ android-app/app/src/main/java/br/com/jmeletrica/orcamentos/MainActivity.kt
@@ -19,7 +19,29 @@ class MainActivity : AppCompatActivity() {
         webView.settings.domStorageEnabled = true
         webView.settings.allowFileAccess = true
         webView.settings.allowContentAccess = true
-        webView.webViewClient = WebViewClient()
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_NEVER_ALLOW
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                val script = """
+                    (function() {
+                        const logoUrl = 'https://raw.githubusercontent.com/johnycristiansan-oss/jm-eletrica-e-contrucoes/main/jm.png';
+                        document.querySelectorAll('.logo').forEach(function(el) {
+                            el.innerHTML = '';
+                            el.style.backgroundImage = 'url(' + logoUrl + ')';
+                            el.style.backgroundSize = 'contain';
+                            el.style.backgroundPosition = 'center';
+                            el.style.backgroundRepeat = 'no-repeat';
+                            el.style.backgroundColor = '#fff';
+                            el.style.border = '0';
+                            el.style.boxShadow = 'none';
+                        });
+                    })();
+                """.trimIndent()
+                view.evaluateJavascript(script, null)
+            }
+        }
         webView.webChromeClient = WebChromeClient()
         webView.setBackgroundColor(0xFFF5F7FA.toInt())
         setContentView(webView)
